5cfa85c3f378dffc2d9d669cda071a8a32f4a51e
ArfinVoldigoads <kinganos50@gmail.com>
2024-12-31 17:33:23 +0700

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    OWNER_ID = "6995317382"
+    sudo_users = "6995317382", "5820365080", "5489586647", "6205444949", "5147271956", "158468045", "6686326398", "5147271956", "7177727796", "5008662958", "7127913934", "5843270062", "1495261563", "6708573850", "6489025882", "6752263178", "5157661772", "6353916710" 
+    GROUP_ID = -1002046761940
+    TOKEN = "7816729390:AAEVikQUOLjfnOkaxxAW5MScriDmRq8hipM"
+    mongo_url = "mongodb+srv://Arfin1:Arfin1@cluster0.5axv7.mongodb.net/?retryWrites=true&w=majority"
+    PHOTO_URL = ["https://telegra.ph/file/8279229551c22fe1c17de.jpg", "https://telegra.ph/file/ddb237bcd33fc09941312.jpg"]
+    SUPPORT_CHAT = "gc_animecommunity"
+    UPDATE_CHAT = "Take_update"
+    BOT_USERNAME = "Waifus_database_bot"
+    CHARA_CHANNEL_ID = "-1002126509901"
+    api_id = 21436816
+    api_hash = "c269918dddddbc041d536207cab72155"
 
     
 class Production(Config):
